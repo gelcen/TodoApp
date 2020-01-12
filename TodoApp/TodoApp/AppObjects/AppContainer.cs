becdce77dea16ec366492d6e8180d388dd3db759
Гелцен Савындай <5842_sgy@keva.int>
2020-01-12 07:30:52 +0700

    Added ioc container class and setup class for the app.

---
+++ TodoApp/TodoApp/AppObjects/AppContainer.cs
@@ -0,0 +1,9 @@
+﻿using Autofac;
+
+namespace TodoApp.AppObjects
+{
+    public static class AppContainer
+    {
+        public static IContainer Container { get; set; }
+    }
+}
---
+++ TodoApp/TodoApp/AppObjects/AppSetup.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+
+namespace TodoApp.AppObjects
+{
+    public class AppSetup
+    {
+        public IContainer CreateContainer()
+        {
+            var cb = new ContainerBuilder();
+            RegisterDependencies(cb);
+            return cb.Build();
+        }
+
+        protected virtual 
+            void RegisterDependencies(ContainerBuilder cb)
+        {
+
+        }
+    }
+}
