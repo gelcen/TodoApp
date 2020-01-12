4a6c8f18b1601a057a6f66edc7d9a59ff8f82f64
Гелцен Савындай <5842_sgy@keva.int>
2020-01-12 07:08:15 +0700

    Добавьте файлы проекта.

---
+++ TodoApp.WPF/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.WPF;
+
+namespace TodoApp.WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : FormsApplicationPage
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Forms.Init();
+            LoadApplication(new TodoApp.App());
+        }
+    }
+}
---
+++ TodoApp/TodoApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TodoApp.Models;
+
+namespace TodoApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
